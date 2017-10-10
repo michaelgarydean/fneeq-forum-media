3de7a8551d0ebac59d8bf4035187703a610753c8
Michael Dean <mike@koumbit.org>
2017-10-10 19:15:17 -0400

    initial commit of plugin files

---
+++ fneeq-forum-media.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Plugin Name:     FNEEQ forum médias
+ * Plugin URI:      https://github.com/mykedean/fneeq-forum-media
+ * Description:     Comportement personnalisé pour les médias ajoutés sur les forums bnpress de FNEEQ.
+ * Author:          Michael Dean
+ * Author URI:      https://github.com/mykedean
+ * Text Domain:     fneeq-forum-media
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Fneeq_Forum_Media
+ */
