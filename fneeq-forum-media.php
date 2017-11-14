81889362c8f38934c52fcfdcea784fc42a555ad5
Michael Dean <mike@koumbit.org>
2017-11-14 14:53:23 -0500

    tagged an attachment uploaded from a topic with the name of it's parent forum. refs #24445

--- fneeq-forum-media.php
+++ fneeq-forum-media.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name:     FNEEQ forum médias
  * Plugin URI:      https://github.com/mykedean/fneeq-forum-media
- * Description:     Comportement personnalisé pour les médias ajoutés sur les forums bnpress de FNEEQ.
+ * Description:     Comportement personnalisé pour les médias ajoutés sur les forums bbpress de FNEEQ.
  * Author:          Michael Dean
  * Author URI:      https://github.com/mykedean
  * Text Domain:     fneeq-forum-media
@@ -11,3 +11,47 @@
  *
  * @package         Fneeq_Forum_Media
  */
+
+/**
+ * This plugin depends on the installation of gd-bbpress-attachments.
+ * https://wordpress.org/plugins/gd-bbpress-attachments/
+ *
+ * @TODO Add verification that GD bbPress Attachments is installed or provide a message to the user if not.
+ * @TODO Error handling if the GD bbPress Attachments plugin is not available.
+ */
+
+
+/**
+ * Assigns a term from the secteur taxonomy to media uploaded from the forums.
+ *
+ * @TODO get the attachment ID
+ * @TODO Test hooks.
+ * @TODO Add error handling in case no term is found for the forum, or create a term for the forum.
+ */
+
+add_action( 'add_attachment', 'fneeq_assign_secteur_to_media' );
+
+function fneeq_assign_secteur_to_media( $attachment_id) {
+        $taxonomy_name = 'category';
+
+	//Get the WP_Post object for the current attachment that has been uploaded.
+	$attachment = get_post( $attachment_id );
+	
+	//Get the WP_Post object of the attachment's parent post
+	$post_parent_id = $attachment->post_parent;
+	$post_parent = get_post( $post_parent_id );
+
+	//If the parent post is a topic, tag the attachment with the name of the topic's forum.
+	if( 'topic' === $post_parent->post_type ) {	
+
+		//For readability. The parent post is a topic.
+		$topic_post = $post_parent;	
+		
+		//Get the forum name of the topic
+		$forum_name = bbp_get_forum( $topic_post->post_parent )->post_name;
+	
+		//Tag the attachment with the name of the forum as a category	
+		var_dump(wp_set_object_terms( $attachment_id, $forum_name, $taxonomy_name, true ));
+		die;
+	}
+}
